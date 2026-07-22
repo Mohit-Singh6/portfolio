8590d3f414103769fa2460b6fea4bd42efcdbe3a
Mohit Singh <mohits.it.24@nitj.ac.in>
2026-07-22 15:43:30 +0530

    Headings

--- components/ui/projectsHeading.tsx
+++ components/ui/projectsHeading.tsx
@@ -4,7 +4,7 @@ import TextAnimation from '@/components/uilayouts/scroll-text'
 export function ProjectsHeading() {
 
     return (
-        <div className='mt-70 mb-[-100]' id='projects'>
+        <div className='lg:mt-70 lg:mb-[-180] md:mt-55 md:mb-[-220] mt-40 mb-[-250]' id='projects'>
             <div className='text-left'>
                 <TextAnimation
                     text="MY PROJECTS"
@@ -18,7 +18,7 @@ export function ProjectsHeading() {
                             transition: { ease: 'linear' },
                         },
                     }}
-                    classname='text-[3.5rem] md:text-[8rem] font-heading  leading-[0.95] tracking-tight text-white text-transparent [-webkit-text-stroke:1px_#eeeeee] opacity-20'
+                    classname='text-[2rem] min-[500px]:text-[3rem] min-[580px]:text-[3.5rem] min-[900px]:text-[4.5rem] min-[1000px]:text-[5.5rem] min-[1300px]:text-[7.9rem] font-heading  leading-[0.95] tracking-tight text-white text-transparent [-webkit-text-stroke:1px_#eeeeee] opacity-20'
                 />
             </div>
         </div>
--- components/ui/skillsHeading.tsx
+++ components/ui/skillsHeading.tsx
@@ -4,7 +4,7 @@ import TextAnimation from '@/components/uilayouts/scroll-text'
 export function IconCloudHeading() {
 
     return (
-        <div className='mt-70'>
+        <div className='md:mt-70 mt-40'>
             <div className='text-left'>
                 <TextAnimation
                     text="MY SKILLS"
@@ -18,7 +18,7 @@ export function IconCloudHeading() {
                             transition: { ease: 'linear' },
                         },
                     }}
-                    classname='text-[6rem] md:text-[8rem] lg:text-[11rem] font-heading  leading-[0.95] tracking-tight text-white text-transparent [-webkit-text-stroke:1px_#eeeeee] opacity-20'
+                    classname='text-[3rem] min-[470px]:text-[4rem] min-[560px]:text-[5rem] min-[800px]:text-[6rem] min-[1000px]:text-[7.9rem] font-heading  leading-[0.95] tracking-tight text-white text-transparent [-webkit-text-stroke:1px_#eeeeee] opacity-20'
                 />
             </div>
         </div>
